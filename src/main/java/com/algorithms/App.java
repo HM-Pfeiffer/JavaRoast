aede77afe9d5317d0f8c6c0b931cc38f769e4fab
heatherpfeiffer <heather.m.pfeiffer@outlook.com>
2025-04-07 15:38:13 -0400

    added switch case

--- src/main/java/com/algorithms/App.java
+++ src/main/java/com/algorithms/App.java
@@ -1,24 +1,43 @@
 package com.algorithms;
 
 import java.util.Arrays;
+import java.util.Scanner; 
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        // 2235. Add Two Integers Sum
-        int num = 121;
-        PalindromeNumber pal = new PalindromeNumber();
-        boolean newPal = pal.isPalindrome(num);
-        System.out.println("Is " + num + " a palindrome number: " + newPal);
-
-        // 1929. Concatenation of Array
-        int[] nums = {1,2,1};
-        ConcatenationOfArray conArr = new ConcatenationOfArray();
-        int[] res = conArr.getConcatenation(nums);
-        System.out.println("result of concatenating " 
-            + Arrays.toString(nums) + " with " 
-            + Arrays.toString(nums)  + " is: " + Arrays.toString(res));
+        Scanner scanner = new Scanner(System.in);
+        int machineOn = 1; 
         
+        while (machineOn == 1){
+            System.out.println("\nEnter the name of the algorithm you want to run: PalindromeNumber, ConcatenationOfArray\n");
+            String algo = scanner.nextLine();
+            
+            switch(algo){
+                case "PalindromeNumber":
+                    int num = 121;
+                    PalindromeNumber pal = new PalindromeNumber();
+                    boolean newPal = pal.isPalindrome(num);
+                    System.out.println("\nIs " + num + " a palindrome number: " + newPal + "\n");
+                    machineOn = 0;
+                    break;
+
+                case "ConcatenationOfArray":
+                    int[] nums = {1,2,1};
+                    ConcatenationOfArray conArr = new ConcatenationOfArray();
+                    int[] res = conArr.getConcatenation(nums);
+                    System.out.println("\nresult of concatenating " 
+                        + Arrays.toString(nums) + " with " 
+                        + Arrays.toString(nums)  + " is: " + Arrays.toString(res) + "\n");
+                    machineOn = 0;
+                    break;
+
+                default:
+                    System.out.println("\nAlgorithm not recognized. Please try again.\n");
+                    break;
+            }
+        }   
+        scanner.close();
     }
 }
